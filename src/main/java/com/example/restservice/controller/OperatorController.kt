60172de4040cb33f0018d0dcbfe2868c8082602c
Parocq <55479321+Parocq@users.noreply.github.com>
2020-10-11 21:40:59 +0300

    Small fixes on controller

--- src/main/java/com/example/restservice/controller/OperatorController.kt
+++ src/main/java/com/example/restservice/controller/OperatorController.kt
@@ -1,58 +1,58 @@
-//package com.example.restservice.controller
-//
-//import com.example.restservice.entity.Operator
-//import com.example.restservice.service.OperatorService
-//import org.springframework.http.HttpHeaders
-//import org.springframework.http.HttpStatus
-//import org.springframework.http.ResponseEntity
-//import org.springframework.web.bind.annotation.*
-//
-//@RestController
-//class OperatorController {
-//
-//    @PutMapping("/operator")
-//    fun insertOperator(
-//            @RequestBody operator: Operator
-//    ): ResponseEntity<Any> {
-//        val httpHeaders = HttpHeaders()
-//
-//        return if(
-//                OperatorService().insertOperator(operator)
-//        ){
-//            ResponseEntity(httpHeaders, HttpStatus.OK)
-//        } else ResponseEntity(httpHeaders, HttpStatus.NOT_FOUND)
-//    }
-//
-//    @GetMapping("/operators")
-//    fun getAllOperators(): List<Operator> {
-//        return OperatorService().getAllOperators()
-//    }
-//
-//    @GetMapping("/operator")
-//    fun getOperator(
-//            @RequestParam(value = "id", defaultValue = "1") id: Int
-//    ): Operator {
-//        return OperatorService().getOperator(id)
-//    }
-//
-//    @GetMapping("/operator")
-//    fun getOperator(
-//            @RequestParam(value = "fio", defaultValue = "default") FIO: String
-//    ): List<Operator> {
-//        return OperatorService().getOperatorsByFIO(FIO)
-//    }
-//
-//    @PatchMapping("/operator")
-//    fun updateOperator(
-//            @RequestParam(value = "id", defaultValue = "1") id: Int,
-//            @RequestBody operator: Operator
-//    ): ResponseEntity<Any> {
-//        val httpHeaders = HttpHeaders()
-//
-//        return if (
-//                OperatorService().updateOperator(id,operator)
-//        ) {
-//            ResponseEntity(httpHeaders, HttpStatus.OK)
-//        } else ResponseEntity(HttpStatus.NOT_FOUND)
-//    }
-//}
+package com.example.restservice.controller
+
+import com.example.restservice.entity.Operator
+import com.example.restservice.service.OperatorService
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class OperatorController {
+
+    @PutMapping("/operator")
+    fun insertOperator(
+            @RequestBody operator: Operator
+    ): ResponseEntity<Any> {
+        val httpHeaders = HttpHeaders()
+
+        return if(
+                OperatorService().insertOperator(operator)
+        ){
+            ResponseEntity(httpHeaders, HttpStatus.OK)
+        } else ResponseEntity(httpHeaders, HttpStatus.NOT_FOUND)
+    }
+
+    @GetMapping("/operators")
+    fun getAllOperators(): List<Operator> {
+        return OperatorService().getAllOperators()
+    }
+
+    @GetMapping("/operator")
+    fun getOperator(
+            @RequestParam(value = "id", defaultValue = "1") id: Int
+    ): Operator {
+        return OperatorService().getOperator(id)
+    }
+
+    @GetMapping("/operator/fio")
+    fun getOperator(
+            @RequestParam(value = "fio", defaultValue = "default") FIO: String
+    ): List<Operator> {
+        return OperatorService().getOperatorsByFIO(FIO)
+    }
+
+    @PatchMapping("/operator")
+    fun updateOperator(
+            @RequestParam(value = "id", defaultValue = "1") id: Int,
+            @RequestBody operator: Operator
+    ): ResponseEntity<Any> {
+        val httpHeaders = HttpHeaders()
+
+        return if (
+                OperatorService().updateOperator(id,operator)
+        ) {
+            ResponseEntity(httpHeaders, HttpStatus.OK)
+        } else ResponseEntity(HttpStatus.NOT_FOUND)
+    }
+}
